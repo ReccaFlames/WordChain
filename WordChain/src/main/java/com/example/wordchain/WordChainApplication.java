624124c83467d14b377edd620c94c5c1690e1d75
Recca-Komputer\Recca <pawczyk81994@gmail.com>
2018-08-26 00:22:41 +0200

    WorChain - H2 Database, simple Insert

--- WordChain/src/main/java/com/example/wordchain/WordChainApplication.java
+++ WordChain/src/main/java/com/example/wordchain/WordChainApplication.java
@@ -1,15 +1,26 @@
 package com.example.wordchain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class WordChainApplication {
+public class WordChainApplication implements CommandLineRunner {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(WordChainApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        jdbcTemplate.execute("DROP TABLE results IF EXISTS ");
+        jdbcTemplate.execute("CREATE TABLE results(id SERIAL, start_word VARCHAR(255), end_word VARCHAR(255), path VARCHAR(255))");
+    }
 }
