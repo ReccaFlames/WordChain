43ad34f009effc7346fd2771f10763edd88a4d28
Recca-Komputer\Recca <pawczyk81994@gmail.com>
2018-08-26 20:38:39 +0200

    WordChain - release version

--- WordChain/src/main/java/com/example/wordchain/Controller/WordChainController.java
+++ WordChain/src/main/java/com/example/wordchain/Controller/WordChainController.java
@@ -1,22 +1,16 @@
 package com.example.wordchain.Controller;
 
 import com.example.wordchain.Model.WordChain;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @Controller
 public class WordChainController {
 
     private final WordChainService service;
-    @Autowired
-    JdbcTemplate jdbcTemplate;
 
     public WordChainController(WordChainService service) {
         this.service = service;
@@ -26,11 +20,6 @@ public class WordChainController {
     public @ResponseBody
     ResponseEntity getWordChain(@RequestParam String startWord, @RequestParam String endWord) {
         WordChain wordChain = service.findWordChain(startWord, endWord);
-        jdbcTemplate.update("insert into results (start_word, end_word, path) " + "values(?, ?, ?)",
-                new Object[]{
-                        wordChain.getStartWord(), wordChain.getEndWord(),
-                        Arrays.stream(wordChain.getChain()).collect(Collectors.joining(","))
-                });
         return ResponseEntity.ok(wordChain);
     }
 
